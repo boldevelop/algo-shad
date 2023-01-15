ae3cb8115e550c5986f536876e94bfac7524836f
Boldakov <boldakov.vladislav@gmail.com>
2023-01-15 14:17:32 +0000

    with cahce and map

--- contest3/d/main.hpp
+++ contest3/d/main.hpp
@@ -1,41 +1,127 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <algorithm>
 #include <string>
+#include <limits>
 
-// #define MAIN FakeMain
-// #ifdef ANSWER
-// #undef MAIN
-// #define MAIN main
-// #endif
+const uint64_t kPrime = 2 * 1'000'000'000 + 11;
+
+struct HashItem {
+    int global_i;
+    int val;
+    int heap_i;
+};
+
+class HashTable {
+    std::vector<std::list<HashItem>> data_;
+    int size_;
+
+public:
+    HashTable() : data_(5000), size_(0) {
+    }
+
+    void Add(const HashItem& item) {
+        data_[Hash(item) % data_.size()].push_back(item);
+        size_++;
+    }
+
+    void Update(const HashItem& item) {
+        auto& list = data_[Hash(item) % data_.size()];
+
+        bool is_exist = false;
+
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            if (it->global_i == item.global_i) {
+                *it = item;
+                is_exist = true;
+                break;
+            }
+        }
+
+        if (!is_exist) {
+            list.push_back(item);
+            size_++;
+        }
+    }
+
+    int Remove(const HashItem& item) {
+        auto& list = data_[Hash(item) % data_.size()];
+
+        int heap_ind = -1;
+
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            if (it->global_i == item.global_i) {
+                heap_ind = it->heap_i;
+                list.erase(it);
+                break;
+            }
+        }
+
+        --size_;
+        return heap_ind;
+    }
+
+    bool Has(const HashItem& item) {
+        auto& list = data_[Hash(item) % data_.size()];
+
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            if (it->global_i == item.global_i) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    int Size() const {
+        return size_;
+    }
+
+private:
+    uint64_t Hash(const HashItem& item) {
+        uint64_t val = item.val;
+        uint64_t ind = item.global_i + 1;
+        uint64_t max_mod = std::numeric_limits<uint64_t>::max() / 2;
+
+        return (val * val + val * kPrime * ind + 42) % max_mod;
+    }
+};
+
+struct HeapItem {
+    int global_i;
+    int val;
+};
 
 class Heap {
     int size_;
-    std::vector<int> data_;
+    std::vector<HeapItem> data_;
+    HashTable h_table_;
+    int last_k_;
 
 public:
-    Heap() : size_(0), data_() {
+    Heap() : size_(0), data_(), h_table_() {
     }
 
-    explicit Heap(const std::vector<int>& inp) : size_(inp.size()), data_(std::move(inp)) {
+    explicit Heap(const std::vector<HeapItem>& inp)
+        : size_(inp.size()), data_(std::move(inp)), h_table_(), last_k_(-1) {
         BuildHeap();
     }
 
-    void Recreate(const std::vector<int>& inp) {
+    void Recreate(const std::vector<HeapItem>& inp) {
         size_ = inp.size();
         data_ = std::move(inp);
         BuildHeap();
     }
 
     int Min() const {
-        return data_[0];
+        return data_[0].val;
     }
 
     int ExtractMin() {
-        auto min = data_[0];
-        std::swap(data_[0], data_[size_ - 1]);
-        data_[size_ - 1] = -1;
-        --size_;
+        auto min = data_[0].val;
+        Swap(0, size_ - 1);
+        RemoveLast();
         SiftDown(0);
         return min;
     }
@@ -43,47 +129,70 @@ public:
     int GetKStatistic(int k_term) {
         int res = -1;
         if (k_term > size_) {
+            last_k_ = -1;
             return res;
         }
-        while (k_term > 0) {
-            res = ExtractMin();
-            --k_term;
+
+        if (last_k_ == -1) {
+            InplaceSort();
+            last_k_ = data_[size_ - k_term].val;
         }
-        return res;
+
+        return last_k_;
     }
 
-    void Remove(int vert) {
+    void Remove(HeapItem item) {
+        if (item.val <= last_k_) {
+            last_k_ = -1;
+        }
+        auto vert = h_table_.Remove({item.global_i, item.val, 0});
         if (vert == 0) {
             ExtractMin();
             return;
         }
-        std::swap(data_[vert], data_[size_ - 1]);
-        data_[size_ - 1] = -1;
-        --size_;
+        Swap(vert, size_ - 1);
+        RemoveLast();
+
         if (size_ == 1) {
             return;
         }
 
         auto parent = (vert - 1) / 2;
-        if (data_[parent] > data_[vert]) {
+        if (data_[parent].val > data_[vert].val) {
             SiftUp(vert);
         } else {
             SiftDown(vert);
         }
     }
 
-    void Add(int val) {
-        if (size_ == static_cast<int>(data_.size())) {
-            data_.push_back(val);
-        } else {
-            data_[size_] = val;
+    void Add(HeapItem item) {
+        if (item.val < last_k_) {
+            last_k_ = -1;
         }
+        if (size_ == static_cast<int>(data_.size())) {
+            data_.push_back(item);
+        } else {
+            data_[size_] = item;
+        }
+
+        h_table_.Add({item.global_i, item.val, size_});
+
         size_++;
         SiftUp(size_ - 1);
     }
 
     bool Empty() const {
         return size_ == 0;
+    }
+
+    void InplaceSort() {
+        auto size = size_;
+        while (size_ > 1) {
+            Swap(0, size_ - 1);
+            --size_;
+            SiftDown(0);
+        }
+        size_ = size;
     }
 
     void Print(int vert = 0, std::string indent = "") const {
@@ -93,7 +202,7 @@ public:
         if (vert >= size_) {
             return;
         }
-        std::cout << indent << data_[vert] << '\n';
+        std::cout << indent << data_[vert].val << '\n';
         indent = vert == 0 ? "'-> " : "    " + indent;
         Print(2 * vert + 1, indent);
         Print(2 * vert + 2, indent);
@@ -102,11 +211,34 @@ public:
         }
     }
 
+    void PrintData() const {
+        for (auto item : data_) {
+            std::cout << item.val << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::vector<HeapItem> GetData() {
+        return data_;
+    }
+
+    std::vector<int> GetValueData() {
+        std::vector<int> data;
+        for (auto item : data_) {
+            data.push_back(item.val);
+        }
+        return data;
+    }
+
+    int MapSize() const {
+        return h_table_.Size();
+    }
+
 private:
     void SiftUp(int vert) {
         auto parent = (vert - 1) / 2;
-        while (vert != 0 && data_[parent] > data_[vert]) {
-            std::swap(data_[parent], data_[vert]);
+        while (vert != 0 && data_[parent].val > data_[vert].val) {
+            Swap(parent, vert);
             vert = parent;
             parent = (vert - 1) / 2;
         }
@@ -117,23 +249,55 @@ private:
             auto left = 2 * vert + 1;
             auto right = 2 * vert + 2;
 
-            auto child = right < size_ && data_[right] < data_[left] ? right : left;
+            auto child = right < size_ && data_[right].val < data_[left].val ? right : left;
 
-            if (data_[vert] <= data_[child]) {
+            if (data_[vert].val <= data_[child].val) {
                 break;
             }
 
-            std::swap(data_[vert], data_[child]);
+            Swap(vert, child);
             vert = child;
         }
     }
 
     void BuildHeap() {
-        for (int i = size_; i >= 0; --i) {
+        for (int i = size_ - 1; i >= 0; --i) {
+            h_table_.Update({data_[i].global_i, data_[i].val, i});
             SiftDown(i);
         }
     }
+
+    void Swap(int src, int dst) {
+        HashItem src_item = {data_[src].global_i, data_[src].val, dst};
+        HashItem dst_item = {data_[dst].global_i, data_[dst].val, src};
+        h_table_.Update(src_item);
+        h_table_.Update(dst_item);
+
+        std::swap(data_[src], data_[dst]);
+    }
+
+    void RemoveLast() {
+        --size_;
+        h_table_.Remove({data_[size_].global_i, data_[size_].val, size_});
+    }
 };
+
+void GetKStatistic(int k_stat, const std::string& directions, const std::vector<int>& data) {
+    auto left_i = 0;
+    auto right_i = 0;
+    Heap heap({{0, data[0]}});
+
+    for (char dir : directions) {
+        if (dir == 'R') {
+            ++right_i;
+            heap.Add({right_i, data[right_i]});
+        } else {
+            heap.Remove({left_i, data[left_i]});
+            ++left_i;
+        }
+        std::cout << heap.GetKStatistic(k_stat) << '\n';
+    }
+}
 
 #ifndef LOCAL
 int main() {
@@ -152,30 +316,35 @@ int main() {
         data[i] = elem;
     }
 
-    std::string directions;
-    std::cin >> directions;
+    auto left_i = 0;
+    auto right_i = 0;
+    Heap heap({{0, data[0]}});
 
-    auto left_it = data.begin();
-    auto right_it = data.begin();
+    std::vector<int> result;
+    result.reserve(query_count);
 
-    int dir_i = 0;
-    while (dir_i < query_count) {
-        auto dir = directions[dir_i];
+    while (query_count > 0) {
+        char dir = std::cin.get();
+
+        if (dir != 'R' && dir != 'L') {
+            continue;
+        }
 
         if (dir == 'R') {
-            ++right_it;
+            ++right_i;
+            heap.Add({right_i, data[right_i]});
         } else {
-            ++left_it;
+            heap.Remove({left_i, data[left_i]});
+            ++left_i;
         }
 
-        if ((right_it - left_it) + 1 < k_stat) {
-            std::cout << -1 << '\n';
-        } else {
-            Heap heap({left_it, right_it + 1});
-            std::cout << heap.GetKStatistic(k_stat) << '\n';
-        }
+        result.push_back(heap.GetKStatistic(k_stat));
 
-        ++dir_i;
+        --query_count;
+    }
+
+    for (auto num : result) {
+        std::cout << num << '\n';
     }
 
     return 0;
--- contest3/d/test.cpp
+++ contest3/d/test.cpp
@@ -1,10 +1,11 @@
 #include <main.hpp>
 #include <catch.hpp>
 #include <util.h>
+#include <functional>
 
 void FillHeap(Heap* p) {
     for (int i = 10; i > 0; --i) {
-        p->Add(i);
+        p->Add({10 - i, i});
         REQUIRE(p->Min() == i);
     }
 }
@@ -12,22 +13,28 @@ void FillHeap(Heap* p) {
 TEST_CASE("Heap Construct from vector") {
     Heap p;
     RandomGenerator rand_gen;
-    std::vector<int> data(10);
-    std::iota(data.begin(), data.end(), 1);
+    std::vector<int> iota_v(10);
+    std::iota(iota_v.begin(), iota_v.end(), 1);
+
+    std::vector<HeapItem> data;
+    for (int i = 0; i < static_cast<int>(iota_v.size()); ++i) {
+        data.push_back({i, iota_v[i]});
+    }
 
     int count = 10;
     while (count > 0) {
         rand_gen.Shuffle(data.begin(), data.end());
         Heap p(data);
         REQUIRE(p.Min() == 1);
+        REQUIRE(p.MapSize() == 10);
 
         for (int i = 0; i < 10; ++i) {
             auto min = p.ExtractMin();
             REQUIRE(min == i + 1);
+            REQUIRE(p.MapSize() == 10 - (i + 1));
         }
         --count;
     }
-
 }
 
 TEST_CASE("Heap extract min") {
@@ -35,7 +42,7 @@ TEST_CASE("Heap extract min") {
         Heap p;
 
         for (int i = 1; i <= 10; ++i) {
-            p.Add(i);
+            p.Add({i - 1, i});
             REQUIRE(p.Min() == 1);
         }
 
@@ -45,13 +52,13 @@ TEST_CASE("Heap extract min") {
         }
         REQUIRE(p.Empty());
 
-        p.Add(4);
+        p.Add({0, 4});
         REQUIRE(p.Min() == 4);
 
-        p.Add(6);
+        p.Add({1, 6});
         REQUIRE(p.Min() == 4);
 
-        p.Add(1);
+        p.Add({2, 1});
         REQUIRE(p.Min() == 1);
     }
 
@@ -65,54 +72,88 @@ TEST_CASE("Heap extract min") {
     REQUIRE(p.Empty());
 }
 
-TEST_CASE("Heap remove") {
-    Heap p;
-    FillHeap(&p);
+// TEST_CASE("Heap remove") {
+//     Heap p;
+//     FillHeap(&p);
 
-    for (int i = 0; i < 9; ++i) {
-        p.Remove(1);
-        REQUIRE(p.Min() == 1);
-    }
-    auto min = p.ExtractMin();
-    REQUIRE(min == 1);
-    REQUIRE(p.Empty());
-}
+//     for (int i = 0; i < 9; ++i) {
+//         p.Remove(1);
+//         REQUIRE(p.Min() == 1);
+//     }
+//     auto min = p.ExtractMin();
+//     REQUIRE(min == 1);
+//     REQUIRE(p.Empty());
+// }
 
 TEST_CASE("Heap k statistics") {
-    Heap p;
     {
         int k = 2;
-        p.Recreate({4, 2});
+        Heap p({{0, 4}});
+
+        p.Add({1, 2});
         REQUIRE(p.GetKStatistic(k) == 4);
 
-        p.Recreate({4, 2, 1});
+        p.Add({3, 1});
         REQUIRE(p.GetKStatistic(k) == 2);
 
-        p.Recreate({2, 1});
+        p.Remove({0, 4});
         REQUIRE(p.GetKStatistic(k) == 2);
 
-        p.Recreate({1});
+        p.Remove({1, 2});
         REQUIRE(p.GetKStatistic(k) == -1);
     }
 
     {
         int k = 1;
-        p.Recreate({1, 2});
+        Heap p({{0, 1}});
+
+        p.Add({1, 2});
         REQUIRE(p.GetKStatistic(k) == 1);
 
-        p.Recreate({2});
+        p.Remove({0, 1});
         REQUIRE(p.GetKStatistic(k) == 2);
 
-        p.Recreate({2, 3});
+        p.Add({2, 3});
         REQUIRE(p.GetKStatistic(k) == 2);
 
-        p.Recreate({3});
+        p.Remove({1, 2});
         REQUIRE(p.GetKStatistic(k) == 3);
 
-        p.Recreate({3, 4});
+        p.Add({3, 4});
         REQUIRE(p.GetKStatistic(k) == 3);
 
-        p.Recreate({4});
+        p.Remove({2, 3});
         REQUIRE(p.GetKStatistic(k) == 4);
+    }
+}
+
+TEST_CASE("Inplace Head sort") {
+    RandomGenerator rand_gen;
+    std::vector<int> correct(9);
+    std::iota(correct.begin(), correct.end(), 1);
+    std::sort(correct.begin(), correct.end(), std::greater<>());
+
+    std::vector<int> initial_val = {7, 4, 2, 6, 1, 5, 3, 8, 9};
+    std::vector<HeapItem> data;
+    for (int i = 0; i < static_cast<int>(initial_val.size()); ++i) {
+        data.push_back({i, initial_val[i]});
+    }
+
+    int count = 10;
+    while (count > 0) {
+        rand_gen.Shuffle(data.begin(), data.end());
+        Heap p(data);
+        REQUIRE(p.Min() == 1);
+        REQUIRE(p.MapSize() == 9);
+
+        p.InplaceSort();
+
+        auto possible_correct = p.GetValueData();
+        REQUIRE(possible_correct.size() == correct.size());
+        for (int i = 0; i < static_cast<int>(possible_correct.size()); ++i) {
+            REQUIRE(possible_correct[i] == correct[i]);
+        }
+
+        --count;
     }
 }
