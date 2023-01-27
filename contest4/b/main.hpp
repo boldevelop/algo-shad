035c8f322024637c41d0a0a4f1f26c6f923be59b
Boldakov <boldakov.vladislav@gmail.com>
2023-01-27 07:43:00 +0000

    correct treap

--- contest4/b/main.hpp
+++ contest4/b/main.hpp
@@ -12,122 +12,128 @@ int GenRandomInt() {
     std::uniform_int_distribution<int> dist(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
     return dist(gen);
 }
+namespace Simple {
+    class Treap {
+        struct Node {
+            int key;
+            int prior;
+            std::shared_ptr<Node> left;
+            std::shared_ptr<Node> right;
 
-class Treap {
-    struct Node {
-        int key;
-        int prior;
-        std::shared_ptr<Node> left;
-        std::shared_ptr<Node> right;
+            Node(int added_key)
+                : key(added_key), prior(GenRandomInt()), left(nullptr), right(nullptr) {
+            }
+        };
 
-        Node(int added_key)
-            : key(added_key), prior(GenRandomInt()), left(nullptr), right(nullptr) {
+        using NodePtr = std::shared_ptr<Node>;
+        NodePtr root_;
+    public:
+        Treap() : root_(nullptr) {
         }
-    };
 
-    using NodePtr = std::shared_ptr<Node>;
-    NodePtr root_;
-public:
-    Treap() : root_(nullptr) {
-    }
+        void Insert(int val) {
+            auto splitted = Split(root_, val);
+            NodePtr new_node = std::make_shared<Node>(val);
+            root_ = Merge(Merge(splitted.left, new_node), splitted.right);
+        }
 
-    void Insert(int val) {
-        auto splitted = Split(root_, val);
-        NodePtr new_node = std::make_shared<Node>(val);
-        root_ = Merge(Merge(splitted.left, new_node), splitted.right);
-    }
-
-    void Remove(int val) {
-        auto splitted = Split(root_, val);
-        if (splitted.elem) {
+        void Remove(int val) {
+            auto splitted = Split(root_, val);
             root_ = Merge(splitted.left, splitted.right);
         }
-    }
 
-    NodePtr Find(int val) {
-        auto splitted = Split(root_, val - 1);
-        return splitted.elem;
-    }
+        NodePtr Find(int val) {
+            auto splitted = Split(root_, val);
+            root_ = Merge(Merge(splitted.left, splitted.elem), splitted.right);
+            return splitted.elem;
+        }
 
-    void Print() const {
-        PrintImpl(root_);
-    }
+        NodePtr UpperBound(int val) {
+            auto splitted = Split(root_, val);
+            root_ = Merge(Merge(splitted.left, splitted.elem), splitted.right);
+            return splitted.right;
+        }
 
-    int GetHeight() const {
-        return GetHeightImpl(root_);
-    }
-    void PrintData(NodePtr node = nullptr) const {
-        PrintDataImpl(node ? node : root_);
-        std::cout << std::endl;
-    }
-private:
-    struct Splitted {
-        NodePtr left;
-        NodePtr elem;
-        NodePtr right;
+        void Print() const {
+            PrintImpl(root_);
+        }
+
+        int GetHeight() const {
+            return GetHeightImpl(root_);
+        }
+        void PrintData(NodePtr node = nullptr) const {
+            PrintDataImpl(node ? node : root_);
+            std::cout << std::endl;
+        }
+    private:
+        struct Splitted {
+            NodePtr left;
+            NodePtr elem;
+            NodePtr right;
+        };
+
+        NodePtr Merge(NodePtr left, NodePtr right) {
+            if (!left || !right) {
+                return left ? left : right;
+            }
+            if (left->prior > right->prior) {
+                left->right = Merge(left->right, right);
+                return left;
+            }
+            right->left = Merge(left, right->left);
+            return right;
+        }
+
+        Splitted Split(NodePtr node, int val) {
+            if (!node) {
+                return {nullptr, nullptr, nullptr};
+            }
+            if (node->key == val) {
+                Splitted splitted{node->left, node, node->right};
+                splitted.elem->left = nullptr;
+                splitted.elem->right = nullptr;
+                return splitted;
+            }
+            if (node->key < val) {
+                auto splitted = Split(node->right, val);
+                node->right = splitted.left;
+                return {node, splitted.elem, splitted.right};
+            }
+            auto splitted = Split(node->left, val);
+            node->left = splitted.right;
+            return {splitted.left, splitted.elem, node};
+        }
+
+        void PrintImpl(NodePtr node, std::string indent = "") const {
+            if (!node) {
+                return;
+            }
+            if (node == root_) {
+                std::cout << "Treap\n";
+            }
+            std::cout << indent << node->key << " (" << node->prior << ')' << '\n';
+            indent = indent == "" ? "'-> " : "    " + indent;
+            PrintImpl(node->left, indent);
+            PrintImpl(node->right, indent);
+        }
+        void PrintDataImpl(NodePtr node) const {
+            if (!node) {
+                return;
+            }
+            PrintDataImpl(node->left);
+            std::cout << node->key << " ";
+            PrintDataImpl(node->right);
+        }
+        int GetHeightImpl(NodePtr node) const {
+            if (!node) {
+                return 0;
+            }
+            auto left = GetHeightImpl(node->left);
+            auto right = GetHeightImpl(node->right);
+            return 1 + std::max(left, right);
+        }
     };
-
-    NodePtr Merge(NodePtr left, NodePtr right) {
-        if (!left || !right) {
-            return left ? left : right;
-        }
-        if (left->prior > right->prior) {
-            left->right = Merge(left->right, right);
-            return left;
-        }
-        right->left = Merge(left, right->left);
-        return right;
-    }
-
-    Splitted Split(NodePtr node, int val) {
-        if (!node) {
-            return {nullptr, nullptr, nullptr};
-        }
-        if (node->key == val) {
-            Splitted splitted{node->left, node, node->right};
-            splitted.elem->left = nullptr;
-            splitted.elem->right = nullptr;
-            return splitted;
-        }
-        if (node->key < val) {
-            auto splitted = Split(node->right, val);
-            node->right = splitted.left;
-            return {node, splitted.elem, splitted.right};
-        }
-        auto splitted = Split(node->left, val);
-        node->left = splitted.right;
-        return {splitted.left, splitted.elem, node};
-    }
-
-    void PrintImpl(NodePtr node, std::string indent = "") const {
-        if (!node) {
-            return;
-        }
-        if (node == root_) {
-            std::cout << "Treap\n";
-        }
-        std::cout << indent << node->key << " (" << node->prior << ')' << '\n';
-        indent = indent == "" ? "'-> " : "    " + indent;
-        PrintImpl(node->left, indent);
-        PrintImpl(node->right, indent);
-    }
-    void PrintDataImpl(NodePtr node) const {
-        if (!node) {
-            return;
-        }
-        PrintDataImpl(node->left);
-        std::cout << node->key << " ";
-        PrintDataImpl(node->right);
-    }
-    int GetHeightImpl(NodePtr node) const {
-        if (!node) {
-            return 0;
-        }
-        auto left = GetHeightImpl(node->left);
-        auto right = GetHeightImpl(node->right);
-        return 1 + std::max(left, right);
-    }
-};
+}
 
 struct Rect {};
 
--- contest4/b/test.cpp
+++ contest4/b/test.cpp
@@ -2,58 +2,76 @@
 #include <main.hpp>
 #include <util.h>
 
-TEST_CASE("Treap Height") {
-    std::vector<int> data(50000);
-    std::iota(data.begin(), data.end(), 1);
+TEST_CASE("Treap", "Simple") {
+    using namespace Simple;
+    SECTION("Height") {
+        std::vector<int> data(100000);
+        std::iota(data.begin(), data.end(), 1);
 
-    Treap treap;
+        Treap treap;
 
-    Log l;
-    for (auto num : data) {
-        treap.Insert(num);
-        treap.Insert(num);
+        Log l;
+        for (auto num : data) {
+            treap.Insert(num);
+        }
+        auto dur = l.GetDuration();
+        std::cout << treap.GetHeight() << ": " << dur << std::endl;
+        REQUIRE(treap.GetHeight() < 50);
     }
-    auto dur = l.GetDuration();
-    std::cout << treap.GetHeight() << ": " << dur << std::endl;
-    REQUIRE(treap.GetHeight() < 50);
-}
 
-TEST_CASE("Treap Insert") {
-    std::vector<int> data(15);
-    std::iota(data.begin(), data.end(), 1);
+    SECTION("Correctness") {
+        std::vector<int> data(15);
+        std::iota(data.begin(), data.end(), 1);
+        RandomGenerator RandGen;
 
-    Treap treap;
+        for (int i = 0; i < 5; ++i) {
+            Treap treap;
 
-    for (auto num : data) {
-        // treap.Insert(num);
-        treap.Insert(num);
+            for (auto num : data) {
+                treap.Insert(num);
+                REQUIRE(treap.Find(num)->key == num);
+                if (i == 0) {
+                    REQUIRE(!treap.UpperBound(num));
+                    REQUIRE(treap.UpperBound(num - 1)->key == num);
+                }
+            }
+
+            for (auto num : data) {
+                REQUIRE(!treap.Find(num + 15));
+                REQUIRE(treap.Find(num)->key == num);
+                REQUIRE(!treap.Find(num - 15));
+                treap.Remove(num + 16);
+            }
+
+            for (auto num : data) {
+                treap.Remove(num);
+                REQUIRE(!treap.Find(num));
+            }
+            REQUIRE(treap.GetHeight() == 0);
+            RandGen.Shuffle(data.begin(), data.end());
+        }
     }
-    treap.PrintData();
-    treap.Print();
-    // treap.Remove(1);
-    for (auto num : data) {
-        std::cout << "REMOVE: " << num << std::endl;
-        treap.Remove(num);
-        treap.PrintData();
+
+    SECTION("Stress") {
+        std::vector<int> data(100000);
+        std::iota(data.begin(), data.end(), 1);
+
+        Treap treap;
+
+        Log logger;
+        for (auto num : data) {
+            Log l;
+            treap.Insert(num);
+            REQUIRE(treap.Find(num)->key == num);
+            REQUIRE(!treap.UpperBound(num));
+            REQUIRE(treap.UpperBound(num - 1)->key == num);
+            auto dur = l.GetDuration();
+            if (dur > 500) {
+                std::cout << treap.GetHeight() << ": " << dur << std::endl;
+            }
+        }
+        auto dur = logger.GetDuration();
+
+        std::cout << "All op: " << dur << std::endl;
     }
-    // treap.PrintData(treap.Find(2));
-    // treap.PrintData(treap.Find(3));
-    // treap.PrintData(treap.Find(4));
-    // treap.PrintData(treap.Find(5));
-    // treap.PrintData(treap.Find(6));
-    std::cout << treap.GetHeight() << std::endl;
-    REQUIRE(true);
-}
-
-TEST_CASE("Treap Insert 2") {
-    std::vector<int> data{1, 2, 2, 2, 2};
-
-    Treap treap;
-
-    for (auto num : data) {
-        treap.Insert(num);
-    }
-    treap.Print();
-    // std::cout << treap.GetHeight() << std::endl;
-    REQUIRE(true);
 }
