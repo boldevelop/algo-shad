514b2cb9d9dd32f324c5a37ecf0ddda01e2f4fb9
Boldakov <boldakov.vladislav@gmail.com>
2023-01-30 11:08:19 +0000

    2 e

--- contest2/e/main.hpp
+++ contest2/e/main.hpp
@@ -1,6 +1,133 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <optional>
+#include <cmath>
+
+struct TimeCost {
+    int64_t val;
+    bool no_solution;
+
+    TimeCost() : val(0), no_solution(true) {
+    }
+
+    TimeCost(int64_t cost, bool no_sol = true) : val(cost), no_solution(no_sol) {
+    }
+
+    bool HasSolution() const {
+        return !no_solution;
+    }
+
+    void SetTime(int64_t time) {
+        val = time;
+        no_solution = false;
+    }
+};
+
+struct Coin {
+    int64_t dist;
+    int64_t time;
+
+    Coin(int64_t di, int64_t ti) : dist(di), time(ti) {
+    }
+
+    int64_t Interval(const Coin& to) const {
+        return std::abs(dist - to.dist);
+    }
+
+    int64_t IsEnoughTime(int64_t p_time) const {
+        return time >= p_time;
+    }
+};
+
+using TimeCostData = std::vector<std::vector<std::vector<std::optional<TimeCost>>>>;
+using Coins = std::vector<Coin>;
+
+TimeCost GetCostFromLR(const TimeCost& left, const TimeCost& right) {
+    if (left.HasSolution() && right.HasSolution()) {
+        return left.val < right.val ? left : right;
+    } else if (left.HasSolution()) {
+        return left;
+    } else if (right.HasSolution()) {
+        return right;
+    }
+    return {};
+}
+
+/* left = 0, right = 1 */
+TimeCost GetTime(int first, int last, bool is_left, const Coins& coins, TimeCostData* time_cost) {
+    if (is_left && (*time_cost)[first][last][0]) {
+        return *(*time_cost)[first][last][0];
+    }
+    if (!is_left && (*time_cost)[first][last][1]) {
+        return *(*time_cost)[first][last][1];
+    }
+    TimeCost left(0);
+    TimeCost right(0);
+
+    if (is_left) {
+        auto time_cost_left = GetTime(first + 1, last, true, coins, time_cost);
+        auto time_cost_right = GetTime(first + 1, last, false, coins, time_cost);
+
+        if (time_cost_left.HasSolution() || time_cost_right.HasSolution()) {
+            int64_t time = 0;
+            if (time_cost_left.HasSolution()) {
+                time = time_cost_left.val + coins[first].Interval(coins[first + 1]);
+                if (coins[first].IsEnoughTime(time)) {
+                    left.SetTime(time);
+                }
+            }
+            if (time_cost_right.HasSolution()) {
+                time = time_cost_right.val + coins[first].Interval(coins[last]);
+                if (coins[first].IsEnoughTime(time)) {
+                    right.SetTime(time);
+                }
+            }
+        }
+    } else {
+        auto time_cost_left = GetTime(first, last - 1, true, coins, time_cost);
+        auto time_cost_right = GetTime(first, last - 1, false, coins, time_cost);
+
+        if (time_cost_left.HasSolution() || time_cost_right.HasSolution()) {
+            int64_t time = 0;
+            if (time_cost_left.HasSolution()) {
+                time = time_cost_left.val + coins[last].Interval(coins[first]);
+                if (coins[last].IsEnoughTime(time)) {
+                    left.SetTime(time);
+                }
+            }
+            if (time_cost_right.HasSolution()) {
+                time = time_cost_right.val + coins[last].Interval(coins[last - 1]);
+                if (coins[last].IsEnoughTime(time)) {
+                    right.SetTime(time);
+                }
+            }
+        }
+    }
+
+    (*time_cost)[first][last][is_left ? 0 : 1] = GetCostFromLR(left, right);
+    return *(*time_cost)[first][last][is_left ? 0 : 1];
+}
+
+std::optional<int64_t> GetAns(Coins& coins) {
+    std::sort(coins.begin(), coins.end(),
+              [](const Coin& lhs, const Coin& rhs) { return lhs.dist < rhs.dist; });
+    auto size = coins.size();
+    TimeCostData time_cost(size, std::vector<std::vector<std::optional<TimeCost>>>(
+                                     size, std::vector<std::optional<TimeCost>>(2)));
+
+    for (int i = 0; i < static_cast<int>(size); ++i) {
+        time_cost[i][i][0] = TimeCost(0, false);
+        time_cost[i][i][1] = TimeCost(0, false);
+    }
+
+    auto res = GetCostFromLR(GetTime(0, coins.size() - 1, true, coins, &time_cost),
+                             GetTime(0, coins.size() - 1, false, coins, &time_cost));
+    if (res.HasSolution()) {
+        return res.val;
+    }
+    return {};
+}
 
 #ifndef LOCAL
 int main() {
@@ -10,7 +137,23 @@ int main() {
     int count;
     std::cin >> count;
 
-    std::cout << "Hello 3 e" << '\n';
+    Coins coins;
+    coins.reserve(count);
+
+    auto iter = count;
+    while (iter > 0) {
+        int64_t dist, time;
+        std::cin >> dist >> time;
+        coins.emplace_back(dist, time);
+        --iter;
+    }
+
+    auto ans = GetAns(coins);
+    if (ans) {
+        std::cout << (*ans) << '\n';
+    } else {
+        std::cout << "No solution" << '\n';
+    }
     return 0;
 }
 
--- contest2/e/test.cpp
+++ contest2/e/test.cpp
@@ -2,6 +2,148 @@
 #include <main.hpp>
 #include <util.h>
 
-TEST_CASE("Пример 1") {
-    REQUIRE(true);
+TEST_CASE("Контест") {
+    SECTION("1 3 5 8 10") {
+        Coins coins{
+            {1, 3},
+            {3, 1},
+            {5, 8},
+            {8, 19},
+            {10, 15}
+        };
+        REQUIRE(*GetAns(coins) == 11);
+    }
+
+    SECTION("1 2 3 4 5") {
+        Coins coins{
+            {1, 5},
+            {2, 1},
+            {3, 4},
+            {4, 2},
+            {5, 3}
+        };
+        REQUIRE(!GetAns(coins));
+    }
+}
+
+TEST_CASE("Свои") {
+    SECTION("1 3 5") {
+        {
+            Coins coins{
+                {1, 6},
+                {3, 1},
+                {5, 2}
+            };
+
+            REQUIRE(*GetAns(coins) == 6);
+        }
+        {
+            Coins coins{
+                {1, 6},
+                {3, 5},
+                {5, 2}
+            };
+
+            REQUIRE(*GetAns(coins) == 4);
+        }
+        {
+            Coins coins{
+                {1, 1},
+                {3, 2},
+                {5, 1}
+            };
+
+            REQUIRE(!GetAns(coins).has_value());
+        }
+    }
+
+    SECTION("1 5 5") {
+        {
+            Coins coins{
+                {1, 1},
+                {5, 4},
+                {5, 1}
+            };
+
+            REQUIRE(!GetAns(coins));
+        }
+        {
+            Coins coins{
+                {1, 1},
+                {5, 1},
+                {5, 4}
+            };
+
+            REQUIRE(!GetAns(coins));
+        }
+        {
+            Coins coins{
+                {1, 1},
+                {5, 4},
+                {5, 4}
+            };
+
+            REQUIRE(*GetAns(coins) == 4);
+        }
+    }
+
+    {
+        Coins coins{
+            {5, 0},
+            {5, 4},
+            {5, 4}
+        };
+
+        REQUIRE(*GetAns(coins) == 0);
+    }
+
+    {
+        Coins coins{
+            {1, 0},
+            {5, 4},
+            {5, 4}
+        };
+
+        REQUIRE(*GetAns(coins) == 4);
+    }
+    {
+        Coins coins{
+            {1, 0},
+            {5, 3},
+            {5, 2}
+        };
+
+        REQUIRE(!GetAns(coins));
+    }
+}
+
+TEST_CASE("Stress") {
+    int size = 1000;
+    std::vector<int64_t> help_data(size);
+    std::iota(help_data.begin(), help_data.end(), 1);
+    {
+        Coins coins;
+        for (auto num : help_data)  {
+            coins.emplace_back(num, num);
+        }
+
+        Log log;
+        auto res = GetAns(coins);
+        auto dur = log.GetDuration();
+        std::cout << "sorted: " << dur << std::endl;
+        REQUIRE(*res == 999);
+    }
+
+    {
+        Coins coins;
+        for (auto num : help_data)  {
+            coins.emplace_back(size - num + 1, size - num + 1);
+        }
+
+        Log log;
+        auto res = GetAns(coins);
+        auto dur = log.GetDuration();
+        std::cout << "non sorted: "<< dur << std::endl;
+        REQUIRE(*res == 999);
+    }
 }
